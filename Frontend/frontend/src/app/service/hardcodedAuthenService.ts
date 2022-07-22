13f69deb96366d2cb8909296f06077d4cf9d911e
len1ck <reversenick@gmail.com>
2022-07-22 17:03:40 +0800

    minor change in login handler

--- Frontend/frontend/src/app/service/hardcodedAuthenService.ts
+++ Frontend/frontend/src/app/service/hardcodedAuthenService.ts
@@ -8,7 +8,6 @@ export class HardcodedAuthenService {
 
   constructor(private apiservice:ApiService) { }
   authenticate(username: string,password: string){
-    //console.log('before'+this.isUserLoggedIn);
       this.apiservice.postAPI("http://127.0.0.1:5000/login",[username,password]).subscribe((data: any) => {
         if(data["message"] == "success"){
           sessionStorage.setItem('authenticaterUser',data["username"]);
