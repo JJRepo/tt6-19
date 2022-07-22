ba1023fc61563cac298676c9f1a1a95464f05bac
woolalal <cedricwookw96@gmail.com>
2022-07-22 15:49:21 +0800

    beautify navbar and login page

--- Frontend/frontend/src/app/footer/footer.component.ts
+++ Frontend/frontend/src/app/footer/footer.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faG } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  faGithub = faGithub
   constructor() { }
 
   ngOnInit(): void {
