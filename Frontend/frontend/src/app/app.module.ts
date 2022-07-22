4cc707ab5a6a7ab467bc2af9fd09958ba4fa6da0
woolalal <cedricwookw96@gmail.com>
2022-07-22 14:16:57 +0800

    create homepage table and btns
    create homepage table and btns

--- Frontend/frontend/src/app/app.module.ts
+++ Frontend/frontend/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
